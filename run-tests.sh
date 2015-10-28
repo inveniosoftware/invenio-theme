2bc2f4ebd6fab25b6ab12e14dd48f8d17075a162
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2015-10-28 16:16:40 +0100

    tests: removal of isort
    * Removes isort from testing utilities as it has different behaviour
      across Python versions.
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -24,7 +24,7 @@
 
 
 pep257 invenio_theme && \
-isort -rc -c -df **/*.py && \
+# isort -rc -c -df **/*.py && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
