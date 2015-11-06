bffdcdb5e8a99f796cbc0ddbdcd83078cd94ce99
Harris Tzovanakis <me@drjova.com>
2015-11-06 17:00:43 +0100

    static: addition of build.js configuration
    * Adds build profile `build.js` needed from `r.js`
      to optiomize all modules and files.
    
    * Moves the `REQUIRE_CONFIG` from `Invenio-Assets` to `Invenio-Theme`.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- invenio_theme/ext.py
+++ invenio_theme/ext.py
@@ -79,6 +79,7 @@ class InvenioTheme(object):
         """Initialize configuration."""
         config.setdefault('SASS_BIN', 'node-sass')
         config.setdefault("THEME_SITENAME", _("Invenio"))
+        config.setdefault("REQUIREJS_CONFIG", 'js/build.js')
         config.setdefault("THEME_GOOGLE_SITE_VERIFICATION", [])
         config.setdefault('BASE_TEMPLATE', 'invenio_theme/page.html')
         config.setdefault(
