97e9eb4dba676f881cfe0a8fc4dc3b3004bd32ca
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-05-16 13:57:45 +0200

    global: initial webpack support

---
+++ invenio_theme/assets/js/invenio_theme/admin.js
@@ -0,0 +1,9 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2016-2018 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+ import jquery from 'jquery/jquery'
+ import select2 from 'select2/dist/js/select2'
---
+++ invenio_theme/assets/js/invenio_theme/base.js
@@ -0,0 +1,9 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2017-2018 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+ // eslint-disable-next-line no-unused-vars
+ import bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap'
