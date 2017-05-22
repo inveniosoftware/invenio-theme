ac9398af013401c843abaa5df1a00ab9cec622d7
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2017-05-22 14:23:59 +0200

    theme: import jQuery
    * Imports jQuery from Webpack.
    
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- invenio_theme/assets/js/invenio_theme/base.js
+++ invenio_theme/assets/js/invenio_theme/base.js
@@ -6,4 +6,6 @@
  * under the terms of the MIT License; see LICENSE file for more details.
  */
  // eslint-disable-next-line no-unused-vars
+ import jquery from 'jquery/dist/jquery'
+ // eslint-disable-next-line no-unused-vars
  import bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap'
