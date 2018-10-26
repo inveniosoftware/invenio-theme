ade4a44eef22c9ac50abe04f43e2393a089e2c02
Jan Okraska <jan.okraska@cern.ch>
2018-10-26 14:17:48 +0200

    global: integrate flask-webpackext
    Signed-off-by: Jan Okraska <jan.okraska@cern.ch>

--- examples/app-setup.sh
+++ examples/app-setup.sh
@@ -11,18 +11,10 @@ DIR=`dirname "$0"`
 cd $DIR
 export FLASK_APP=app.py
 
-# Install requirements
-npm install -g node-sass clean-css clean-css-cli requirejs uglify-js
-
-# Collect npm, requirements from registered bundles
-flask npm
-
-# Now install npm requirements (requires that npm is already installed)
-cd static ; npm install ; cd ..
 
 # Next, we copy the static files from the Python packages into the Flask
 # application's static folder
 flask collect -v
 
-# Finally, we build the webassets bundles
-flask assets build
+flask webpack buildall
+
