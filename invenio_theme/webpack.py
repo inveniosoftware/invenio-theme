f2c05b47e130dbfd25a1a9b6fca1e366d6405155
Chiara Bigarella <chiara.bigarella1@gmail.com>
2018-11-02 13:55:04 +0100

    webpack: move WebpackBundle to separate file

---
+++ invenio_theme/webpack.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""JS/CSS bundles for theme.
+
+You include one of the bundles in a page like the example below (using
+``base`` bundle as an example):
+
+.. code-block:: html
+
+    {{ webpack['base.js']}}
+
+"""
+
+from __future__ import absolute_import, print_function
+
+from flask_webpackext import WebpackBundle
+
+theme = WebpackBundle(
+    __name__,
+    'assets',
+    entry={
+        'base': './js/invenio_theme/base.js',
+        # requires jquery, momoent, select2, bootstrap
+        'adminlte': 'admin-lte/dist/js/app.js',
+        'theme-admin': './scss/invenio_theme/admin.scss',
+        'theme': './scss/invenio_theme/styles.scss',
+    },
+    dependencies={
+        'bootstrap-sass': '~3.3.5',
+        'font-awesome': '~4.4.0',
+        'jquery': '~3.2.1',
+        'moment': '~2.9.0',
+        'select2': '~4.0.2',
+        'admin-lte': '~2.3.11',
+    }
+)
