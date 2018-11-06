7b05145d99629168d68a0830fb3e3f62b253bcd2
Chiara Bigarella <chiara.bigarella1@gmail.com>
2018-11-06 10:02:52 +0100

    assets: fix js files according to prettier

--- invenio_theme/assets/js/invenio_theme/admin.js
+++ invenio_theme/assets/js/invenio_theme/admin.js
@@ -5,9 +5,9 @@
  * Invenio is free software; you can redistribute it and/or modify it
  * under the terms of the MIT License; see LICENSE file for more details.
  */
- // eslint-disable-next-line no-unused-vars
- import jquery from 'jquery/dist/jquery'
- // eslint-disable-next-line no-unused-vars
- import bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap'
- // eslint-disable-next-line no-unused-vars
- import select2 from 'select2/dist/js/select2'
+// eslint-disable-next-line no-unused-vars
+import jquery from "jquery/dist/jquery";
+// eslint-disable-next-line no-unused-vars
+import bootstrap from "bootstrap-sass/assets/javascripts/bootstrap";
+// eslint-disable-next-line no-unused-vars
+import select2 from "select2/dist/js/select2";
