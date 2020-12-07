f11b29a53fabbb3f764ff0bcaef93336dbe5fd45
jrcastro2 <jrcastro9515@gmail.com>
2020-12-07 16:10:59 +0100

    styles: Adds responsive navbar

--- invenio_theme/assets/semantic-ui/js/invenio_theme/theme.js
+++ invenio_theme/assets/semantic-ui/js/invenio_theme/theme.js
@@ -21,3 +21,16 @@ jquery(".message .close").on("click", function () {
     interval: 0,
   });
 });
+
+/* Expand and collapse navbar  */
+const toggle = document.querySelector(".menu-icon");
+const menu = document.querySelector(".navbar-menu");
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+    } else {
+        menu.classList.add("active");
+    }
+}
+toggle.addEventListener("click", toggleMenu, false);
