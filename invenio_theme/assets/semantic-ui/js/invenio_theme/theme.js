9271a88519cfba395523248eb488a65501dbea78
Jenny Bonsak <jenny.bonsak@gmail.com>
2022-07-12 09:53:03 +0200

    theme: revert image placeholder fix due to bug

--- invenio_theme/assets/semantic-ui/js/invenio_theme/theme.js
+++ invenio_theme/assets/semantic-ui/js/invenio_theme/theme.js
@@ -12,13 +12,6 @@ import "./truncate.js";
 import "semantic-ui-css/semantic.js";
 import "semantic-ui-less/semantic.less";
 
-function imagePlaceholder() {
-  this.onerror=null;
-  this.src='/static/images/square-placeholder.png';
-}
-
-jquery(".has-placeholder").attr('onerror', imagePlaceholder);
-
 // Initialize Semantic UI components
 jquery(".ui.dropdown").dropdown();
 
