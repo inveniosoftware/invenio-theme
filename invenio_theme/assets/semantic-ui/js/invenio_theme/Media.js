2016773d07c572a4262201ed224577ec55884e16
kpsherva <karolina.m.przerwa@gmail.com>
2022-08-31 11:06:30 +0200

    global: add Media component for react

---
+++ invenio_theme/assets/semantic-ui/js/invenio_theme/Media.js
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2017-2022 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import { createMedia } from "@artsy/fresnel";
+
+// IMPORTANT: align changes with site.variables
+export const breakpoints = {
+  mobile: 320,
+  tablet: 768,
+  computer: 1300,
+  largeScreen: 1680,
+  widescreen: 1920,
+};
+
+export const AppMedia = createMedia({
+  breakpoints: breakpoints,
+});
+
