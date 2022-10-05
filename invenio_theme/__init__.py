44931c38845c49ad08fe0c11e0c89279d5920463
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-10-05 11:02:24 +0200

    release: v1.4.0

--- invenio_theme/__init__.py
+++ invenio_theme/__init__.py
@@ -522,6 +522,6 @@ from __future__ import absolute_import, print_function
 
 from .ext import InvenioTheme
 
-__version__ = "1.3.31"
+__version__ = "1.4.0"
 
 __all__ = ("__version__", "InvenioTheme")
