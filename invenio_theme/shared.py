92a5c888f9652553ebe9cd1abeaaf2f2b7dedc38
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-06-11 23:09:24 +0200

    global: introduce shared menu
    * the main point of using a shared menu is to have the menu outside of
      the application context

---
+++ invenio_theme/shared.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2023 Graz University of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio standard theme."""
+
+from flask_menu.menu import MenuRoot
+
+menu = MenuRoot("", None)
