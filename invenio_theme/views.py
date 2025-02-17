cfd0af4775d48e0d608f987bbfa7e0c161442ca9
Guillaume Viger <fenekku@fenekku.com>
2025-02-17 14:11:14 -0500

    blueprints: get rather than [] THEME_FRONTPAGE

--- invenio_theme/views.py
+++ invenio_theme/views.py
@@ -21,7 +21,7 @@ def create_blueprint(app):
         static_folder="static",
     )
 
-    if app.config["THEME_FRONTPAGE"]:
+    if app.config.get("THEME_FRONTPAGE"):
         blueprint.add_url_rule("/", "index", view_func=index)
 
     app.register_error_handler(401, unauthorized)
