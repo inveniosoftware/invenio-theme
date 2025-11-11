cb2f9f2c6ffde86fa30dd8097f25aca4c6c4db78
Guillaume Viger <fenekku@users.noreply.github.com>
2025-11-11 13:32:13 -0500

    tests: extend support to Python 3.14

--- docs/conf.py
+++ docs/conf.py
@@ -316,7 +316,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/": None,
+    "python": ("https://docs.python.org/", None),
     "flask": ("http://flask.pocoo.org/docs/latest/", None),
     "flask_menu": ("https://flask-menu.readthedocs.io/en/latest/", None),
     "invenio_assets": ("https://invenio-assets.readthedocs.io/en/latest/", None),
