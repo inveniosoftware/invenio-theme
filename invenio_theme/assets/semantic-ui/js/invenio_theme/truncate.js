4472af2331900ef5031eb26c889b7a1a9a8f0144
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-22 23:12:13 +0200

    chore(licenses): update license headers to use SPDX

--- invenio_theme/assets/semantic-ui/js/invenio_theme/truncate.js
+++ invenio_theme/assets/semantic-ui/js/invenio_theme/truncate.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022 CERN.
+ * SPDX-License-Identifier: MIT
+ */
+
 import $ from "jquery";
 
 (function () {
--- invenio_theme/ext.py
+++ invenio_theme/ext.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
-# Copyright (C) 2022-2023 Graz University of Technology.
-# Copyright (C) 2025 Northwestern University.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2015-2018 CERN.
+# SPDX-FileCopyrightText: 2022-2023 Graz University of Technology.
+# SPDX-FileCopyrightText: 2025 Northwestern University.
+# SPDX-License-Identifier: MIT
 
 """Invenio standard theme."""
 
